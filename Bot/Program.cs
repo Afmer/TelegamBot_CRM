c920323f9c00aad4024d81741ecbfb05ed991a9f
Afmer <afmerlord@gmail.com>
2023-11-19 14:04:11 +0300

    Add bot working

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -5,6 +5,8 @@ public static class Program
 {
     static void Main()
     {
-        
+        var config = new ConfigurationService();
+        var bot = new TelegramBot(config.TelegramBotConfiguration);
+        Console.ReadLine();
     }
 }
--- Bot/TelegramBot.cs
+++ Bot/TelegramBot.cs
@@ -1,13 +1,14 @@
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using TelegramBot_CRM.Architecture.Configurations;
 namespace TelegramBot_CRM;
 public class TelegramBot
 {
     private readonly ITelegramBotClient bot;
-    public TelegramBot(string token)
+    public TelegramBot(TelegramBotConfiguration configuration)
     {
-        bot = new TelegramBotClient(token);
+        bot = new TelegramBotClient(configuration.Token);
         try
         {
             var name = bot.GetMeAsync().Result.FirstName;
@@ -32,7 +33,18 @@ public class TelegramBot
     }
     private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+        {
+            var message = update.Message;
+            if (message != null && message.Text != null)
+            {
+                if(message.Text == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Инициализация пройдена");
+                }
+            }
+        }
     }
     private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
