267cea8a56369f4b09e294383cbb30de7e568765
Afmer <afmerlord@gmail.com>
2023-11-19 17:57:08 +0300

    Refactoring

--- Bot/TelegramBot.cs
+++ Bot/TelegramBot.cs
@@ -60,44 +60,19 @@ public class TelegramBot
         var words = command.Split(' ');
         if(words[0] == "/start")
         {
-            await botClient.SendTextMessageAsync(message.Chat, "Приветствую вас! Напишите /help для более подробной информации о моих возможностях");
+            await StartCommand(botClient, message);
         }
         else if(words[0] == "/inn")
         {
-            var text = "";
-            for(int i = 1; i < words.Length; i++)
-            {
-                var client = await _innApi.GetUserByInn(words[i]);
-                if(client.Status == GetUserByInnStatus.InvalidInn)
-                    text += $"{words[i]} не является ИНН";
-                else if(client.Status == GetUserByInnStatus.NotFound)
-                {
-                    text += $"{words[i]} не найден";
-                }
-                else
-                {
-                    text += $"{client.Client!.Name}\n{client.Client!.Address}"; 
-                }
-                if(i + 1 < words.Length)
-                    text += "\n\n";
-            }
-            if(words.Length == 1)
-                text = "Вы не ввели ни одного ИНН";
-            await botClient.SendTextMessageAsync(message.Chat, text);
+            await InnCommand(words, botClient, message);
         }
         else if(words[0] == "/last")
         {
-            string username = message.From!.Username!;
-            var commandState = _stateMachine.GetLastMessageState(username);
-            if(commandState == null)
-                return;
-            await CommandHandler(commandState, botClient, message, true);
+            await LastCommand(botClient, message, isLastCommandExecute);
         }
         else if(words[0] == "/help")
         {
-            string text = "Введите \"/inn [ИНН 1] [ИНН 2] ... [ИНН n]\", чтобы получить компании, которым принадлежат введенные вами ИНН" +
-                "\n\nВведите \"/last\", чтобы выполнить последнюю введенную вами команду";
-            await botClient.SendTextMessageAsync(message.Chat, text);
+            await HelpCommand(botClient, message);
         }
         else
         {
@@ -109,5 +84,47 @@ public class TelegramBot
             string username = message.From!.Username!;
             _stateMachine.SetLastMessageState(username, message.Text!);
         }
+    }
+
+    private async Task StartCommand(ITelegramBotClient botClient, Message message)
+    {
+        await botClient.SendTextMessageAsync(message.Chat, "Приветствую вас! Напишите /help для более подробной информации о моих возможностях");
+    }
+    private async Task InnCommand(string[] words, ITelegramBotClient botClient, Message message)
+    {
+        var text = "";
+        for(int i = 1; i < words.Length; i++)
+        {
+            var client = await _innApi.GetUserByInn(words[i]);
+            if(client.Status == GetUserByInnStatus.InvalidInn)
+                text += $"{words[i]} не является ИНН";
+            else if(client.Status == GetUserByInnStatus.NotFound)
+            {
+                text += $"{words[i]} не найден";
+            }
+            else
+            {
+                text += $"{client.Client!.Name}\n{client.Client!.Address}"; 
+            }
+            if(i + 1 < words.Length)
+                text += "\n\n";
+        }
+        if(words.Length == 1)
+            text = "Вы не ввели ни одного ИНН";
+        await botClient.SendTextMessageAsync(message.Chat, text);
+    }
+    private async Task LastCommand(ITelegramBotClient botClient, Message message, bool isLastCommandExecute)
+    {
+        string username = message.From!.Username!;
+        var commandState = _stateMachine.GetLastMessageState(username);
+        if(commandState == null)
+            return;
+        await CommandHandler(commandState, botClient, message, true);
+    }
+    private async Task HelpCommand(ITelegramBotClient botClient, Message message)
+    {
+        string text = "Введите \"/inn [ИНН 1] [ИНН 2] ... [ИНН n]\", чтобы получить компании, которым принадлежат введенные вами ИНН" +
+            "\n\nВведите \"/last\", чтобы выполнить последнюю введенную вами команду";
+        await botClient.SendTextMessageAsync(message.Chat, text);
     }
 }
